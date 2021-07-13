a21ae5cba99d88c3e57e9f3c270011607fdc507c
Markus Muenzel <markus@muenzel.de>
2021-07-13 23:47:27 +0200

    readded check for blacklisted domain for function discoverHomeserverUrl

--- src/matrixUtils.js
+++ src/matrixUtils.js
@@ -54,6 +54,11 @@ function parseHostnameAndPort(serverName) {
 async function discoverHomeserverUrl(serverName) {
     let {hostname, port, defaultPort} = parseHostnameAndPort(serverName);
 
+    // Don't continue if we consider the hostname part to resolve to our blacklisted IP ranges
+    if (utils.isBlacklisted(await utils.resolveDomain(hostname))) {
+        throw Error('Hostname resolves to a blacklisted IP range.');
+    }
+
     /**
      * 1. If the hostname is an IP literal, then that IP address should be used, together with the given port number,
      * or 8448 if no port is given. The target server must present a valid certificate for the IP address.
